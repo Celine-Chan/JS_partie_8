07b9720e0c5d27b06c3b442ad75ad1b5e68f7c54
Celine_lhp5 <celine.louvel.76@outlook.fr>
2020-11-16 13:39:11 +0100

    ajout ok

---
+++ exercice-01/assets/js/script.js
@@ -0,0 +1,12 @@
+let arrowTop = document.getElementById("animateButton");
+// ou const arrowTop = document.getElementById("animateButton");
+
+function topPage() {
+    document.documentElement.scrollTop = 0;
+}
+
+
+// correction
+// arrowTop.onclick = function() {
+//     document.documentElement.scrollTop = 0;
+// }
